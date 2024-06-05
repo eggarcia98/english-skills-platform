fb8cd7dd832a21ed3bbde7e561df103ba23f2cea
Erick Garcia <eggarcia9814@gmail.com>
2024-06-05 17:24:31 +1000

    Fixing UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,14 @@ export default function Home() {
     const [audioFile, setAudioFile] = useState(null);
     const [audioUrl, setAudioUrl] = useState(null);
 
-    const [fragmetAudioTime, setFragmentAudioTime] = useState()
+    const [fragmetAudioTime, setFragmentAudioTime] = useState();
 
     return (
-        <main className=" min-h-screen flex-col items-center ">
-            
-            <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                English Speaking Activities
-            </p>
-            <div className="sm:flex  p-24 items-center justify-between font-mono text-sm">
+        <main className="grid grid-cols-2 grid-rows-[auto_auto_auto_auto] h-fit justify-items-center">
+            <div className="col-span-2">
+                <p className="">English Speaking Activities</p>
+            </div>
+            <div className=" col-span-2 justify-center">
                 <UploadFile setAudioFile={setAudioFile} />
                 <AudioUrlInput setAudioUrl={setAudioUrl} />
             </div>
