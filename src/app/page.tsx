dc593c05d96fdfdd71a7178b8a4073a7b063362f
Erick Garcia <eggarcia9814@gmail.com>
2024-06-06 16:48:51 +1000

    Components position fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,31 +13,33 @@ export default function Home() {
     const [fragmetAudioTime, setFragmentAudioTime] = useState();
 
     return (
-        <main className="grid grid-cols-2 grid-rows-[auto_auto_auto_auto] h-fit justify-items-center gap-6 space-evenly m-5">
-            <div className="col-span-2 m-2">
-                <p className="">English Speaking Activities</p>
-            </div>
-            <div className="col-span-2 w-full">
-                <div className="flex flex-col w-full items-center ">
-                    <UploadFile setAudioFile={setAudioFile} />
-                    <AudioUrlInput setAudioUrl={setAudioUrl} />
+        <div className="h-screen">
+            <main className="grid grid-cols-2 grid-rows-[auto_auto_auto_auto] h-fit justify-items-center gap-6 space-evenly m-5">
+                <div className="col-span-2 m-2">
+                    <p className="">English Speaking Activities</p>
                 </div>
-            </div>
+                <div className="col-span-2 w-full">
+                    <div className="flex flex-col w-full items-center ">
+                        <UploadFile setAudioFile={setAudioFile} />
+                        <AudioUrlInput setAudioUrl={setAudioUrl} />
+                    </div>
+                </div>
 
-            <div className="w-full">
-                <ReactPlayerComponent
-                    audioFile={audioFile}
-                    audioUrl={audioUrl}
-                    fragmetAudioTime={fragmetAudioTime}
-                />
-            </div>
-            <div className="w-full max-w-lg ">
-                <ExerciseBoard
-                    audioFile={audioFile}
-                    audioUrl={audioUrl}
-                    setFragmentAudioTime={setFragmentAudioTime}
-                />
-            </div>
-        </main>
+                <div className="w-full ">
+                    <ReactPlayerComponent
+                        audioFile={audioFile}
+                        audioUrl={audioUrl}
+                        fragmetAudioTime={fragmetAudioTime}
+                    />
+                </div>
+                <div className="w-full  ">
+                    <ExerciseBoard
+                        audioFile={audioFile}
+                        audioUrl={audioUrl}
+                        setFragmentAudioTime={setFragmentAudioTime}
+                    />
+                </div>
+            </main>
+        </div>
     );
 }
--- src/components/ExerciseCards.tsx
+++ src/components/ExerciseCards.tsx
@@ -78,23 +78,22 @@ export default function ExerciseCards({
     };
 
     return (
-        <div className="relative font-inter antialiased">
-            <div className="relative min-h-screen flex flex-col justify-center bg-slate-900 overflow-hidden">
-                <div className="w-full max-w-5xl mx-auto px-4 md:px-6 py-24">
-                    <section className="px-12">
-                        <div className="max-w-lg mx-auto relative">
-                            {cardsData.map(
-                                (
-                                    { transcript, ...rest }: AudioSummary,
-                                    index: number
-                                ) => (
-                                    <div
-                                        key={index}
-                                        className={`
+        <div className=" overflow-hidden">
+            <div className="w-full max-w-5xl mx-auto px-4 md:px-6 py-5">
+                <section className="px-12">
+                    <div className="max-w-lg mx-auto relative">
+                        {cardsData.map(
+                            (
+                                { transcript, ...rest }: AudioSummary,
+                                index: number
+                            ) => (
+                                <div
+                                    key={index}
+                                    className={`
                                         absolute inset-0 scale-[67.5%] z-20 transition-all duration-500 ease-[cubic-bezier(0.25,1,0.5,1)]
                                         ${
                                             currentIndex === index
-                                                ? "relative z-50 translate-x-0 scale-[100%]"
+                                                ? "relative z-50 translate-x-0 scale-[103%]"
                                                 : ""
                                         }
                                         ${
@@ -121,69 +120,68 @@ export default function ExerciseCards({
                                          }
                                         
                                     `}
-                                    >
-                                        <article className="bg-white p-6 rounded-lg shadow-2xl">
-                                            <header className="mb-5">
-                                                <h2 className="block antialiased tracking-normal font-sans text-4xl font-semibold leading-[1.3] text-slate-900 mb-4">
-                                                    Exercise ({index + 1}/
-                                                    {cardsData.length})
-                                                </h2>
-                                            </header>
-                                            <div className="text-sm leading-relaxed text-slate-500 space-y-4 mb-2">
-                                                {/* <label
+                                >
+                                    <article className="bg-white p-6 rounded-lg shadow-2xl">
+                                        <header className="mb-5">
+                                            <h2 className="block antialiased tracking-normal font-sans sm:text-3xl text-4xl font-semibold leading-[1.3] text-slate-900 mb-4">
+                                                Exercise ({index + 1}/
+                                                {cardsData.length})
+                                            </h2>
+                                        </header>
+                                        <div className="text-sm leading-relaxed text-slate-500 space-y-4 mb-2">
+                                            {/* <label
                                                 htmlFor="message"
                                                 className="block mb-2 text-sm justify-items-end font-medium text-gray-900 dark:text-gray-400"
                                             >
                                                 Words to guess: 
                                             </label> */}
-                                                <textarea
-                                                    id="message"
-                                                    rows={4}
-                                                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                    placeholder="What did you here?"
-                                                ></textarea>
+                                            <textarea
+                                                id="message"
+                                                rows={4}
+                                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                placeholder="What did you here?"
+                                            ></textarea>
 
-                                                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                                    <button
-                                                        type="button"
-                                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-700 text-base font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                                        onClick={() =>
-                                                            setFragmentAudioTime(
-                                                                rest
-                                                            )
-                                                        }
-                                                    >
-                                                        Play ▷
-                                                    </button>
-                                                    <button
-                                                        type="button"
-                                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                                                    >
-                                                        Show hints
-                                                    </button>
-                                                </div>
+                                            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                                <button
+                                                    type="button"
+                                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-700 text-base font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                                    onClick={() =>
+                                                        setFragmentAudioTime(
+                                                            rest
+                                                        )
+                                                    }
+                                                >
+                                                    Play ▷
+                                                </button>
+                                                <button
+                                                    type="button"
+                                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                                >
+                                                    Show hints
+                                                </button>
                                             </div>
-                                            <footer className="flex justify-between">
-                                                <button
-                                                    className="text-sm font-medium text-indigo-500 hover:underline "
-                                                    onClick={handlePrevious}
-                                                >
-                                                    Previous
-                                                </button>
-                                                <button
-                                                    className="text-sm font-medium text-indigo-500 hover:underline"
-                                                    onClick={handleNext}
-                                                >
-                                                    Next
-                                                </button>
-                                            </footer>
-                                        </article>
-                                    </div>
-                                )
-                            )}
-                        </div>
-                    </section>
-                </div>
+                                        </div>
+                                        <footer className="flex justify-between">
+                                            <button
+                                                className="text-sm font-medium text-indigo-500 hover:underline "
+                                                onClick={handlePrevious}
+                                            >
+                                                Previous
+                                            </button>
+                                            <button
+                                                className="text-sm font-medium text-indigo-500 hover:underline"
+                                                onClick={handleNext}
+                                            >
+                                                Next
+                                            </button>
+                                        </footer>
+                                    </article>
+                                </div>
+                            )
+                        )}
+                    </div>
+                </section>
             </div>
         </div>
     );
