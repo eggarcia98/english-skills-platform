8a7e63d88300e82d81eb9a4b2e5d327352600760
Erick Garcia <eggarcia9814@gmail.com>
2024-09-17 20:56:18 +1000

    Home text size modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import AudioUrlInput from "@/components/Audio/AudioUrlInput";
 import { FragmentAudioData } from "@/types";
 
 export default function Home() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [activeSection, setActiveSection] = useState("home"); // State to track the active section
+    const titlePage = "Audio Learner";
+
     const [audioFile, setAudioFile] = useState(null);
     const [audioUrl, setAudioUrl] = useState(null);
 
@@ -15,36 +19,162 @@ export default function Home() {
         {} as FragmentAudioData
     );
 
+    const handleMenuClick = (section: string) => {
+        setActiveSection(section);
+        setIsMenuOpen(false); // Close the mobile menu after clicking
+    };
+
+    const menuItems = () => <div></div>;
+
     return (
-        <div className="h-screen  bg-slate-50">
-            <div className="sticky bg-slate-50 top-0 col-span-2 w-full h-16 flex justify-center border-b-[0.01rem] border-gray-600">
-                <p className="place-self-center text-center font-mono text-lg dark:text-black">
-                    🗣️ English Speaking Activities
-                </p>
-            </div>
-            <main className="grid grid-cols-2 grid-rows-[auto_auto_auto_auto] justify-items-center gap-6 space-evenly m-5">
-                <div className="w-full col-span-2 ">
-                    <div className="flex flex-col w-full items-center ">
-                        <UploadFile setAudioFile={setAudioFile} />
-                        <AudioUrlInput setAudioUrl={setAudioUrl} />
+        <div className="min-h-screen flex flex-col text-gray-800">
+            {/* Navigation */}
+            <nav className="border-b border-gray-200 bg-white py-4 n-xs:px-44 px-4">
+                <div className="container mx-auto flex justify-between items-center">
+                    <h1 className="text-xl font-semibold tracking-wide">
+                        {titlePage}
+                    </h1>
+
+                    {/* Hamburger Icon for Mobile */}
+                    <div className="lg:hidden">
+                        <button
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                            className="focus:outline-none"
+                        >
+                            <svg
+                                className="w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d={
+                                        isMenuOpen
+                                            ? "M6 18L18 6M6 6l12 12"
+                                            : "M4 6h16M4 12h16m-7 6h7"
+                                    }
+                                />
+                            </svg>
+                        </button>
                     </div>
+
+                    {/* Menu Items */}
+                    {/* Desktop Menu Items */}
+                    <ul className="lg:flex space-x-6 text-sm hidden">
+                        <li>
+                            <button
+                                onClick={() => handleMenuClick("home")}
+                                className="hover:text-blue-600 transition-colors duration-300"
+                            >
+                                Home
+                            </button>
+                        </li>
+                        <li>
+                            <button
+                                onClick={() => handleMenuClick("exercises")}
+                                className="hover:text-blue-600 transition-colors duration-300"
+                            >
+                                Exercises
+                            </button>
+                        </li>
+                    </ul>
                 </div>
 
-                <div className="w-full col-span-2 lg:col-span-1">
-                    <ReactPlayerComponent
-                        audioFile={audioFile}
-                        audioUrl={audioUrl}
-                        fragmetAudioTime={fragmetAudioTime}
-                    />
-                </div>
-                <div className="w-full col-span-2 lg:col-span-1">
-                    <ExerciseBoard
-                        audioFile={audioFile}
-                        audioUrl={audioUrl}
-                        setFragmentAudioTime={setFragmentAudioTime}
-                    />
+                {/* Mobile Menu */}
+                {isMenuOpen && (
+                    <div className="lg:hidden">
+                        <ul className="space-y-4 text-center mt-4">
+                            <li>
+                                <a
+                                    href="/"
+                                    onClick={() => handleMenuClick("home")}
+                                    className="block text-gray-700 hover:text-blue-600"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="/exercises"
+                                    onClick={() => handleMenuClick("exercises")}
+                                    className="block text-gray-700 hover:text-blue-600"
+                                >
+                                    Excersise
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </nav>
+
+            {/* Hero Section */}
+            <main className="flex-grow bg-gray-50 n-xs:px-48 sm:px-4 px-4 flex items-center justify-center">
+                <div className="container mx-auto text-center">
+                    {activeSection === "home" && (
+                        <div>
+                            <h2 className="text-3xl font-bold text-gray-900 leading-tight mb-4">
+                                Simple, Customizable, and Easy to use
+                            </h2>
+                            <p className="text-lg text-gray-600 mb-6 max-w-2xl mx-auto">
+                                <br />
+                                This tool helps improve your English listening
+                                skills using your favorite series or even your
+                                own audio files.
+                                <br />
+                                <br />
+                                All you need is a YouTube link to the video or
+                                audio you want to practice with, or you can
+                                upload an audio file directly from your device
+                            </p>
+                            <button
+                                onClick={() => handleMenuClick("exercises")}
+                                className="px-8 py-3 bg-blue-600 text-white text-sm rounded-md shadow-sm hover:bg-blue-700 transition-all duration-300"
+                            >
+                                Get Started
+                            </button>
+                        </div>
+                    )}
+                    {activeSection === "exercises" && (
+                        <div className="grid grid-cols-2 grid-rows-[auto_auto_auto_auto] justify-items-center gap-6 space-evenly m-5">
+                            <div className="w-full col-span-2 ">
+                                <div className="flex flex-col w-full items-center ">
+                                    <UploadFile setAudioFile={setAudioFile} />
+                                    <AudioUrlInput setAudioUrl={setAudioUrl} />
+                                </div>
+                            </div>
+
+                            <div className="w-full col-span-2 lg:col-span-1">
+                                <ReactPlayerComponent
+                                    audioFile={audioFile}
+                                    audioUrl={audioUrl}
+                                    fragmetAudioTime={fragmetAudioTime}
+                                />
+                            </div>
+                            <div className="w-full col-span-2 lg:col-span-1">
+                                <ExerciseBoard
+                                    audioFile={audioFile}
+                                    audioUrl={audioUrl}
+                                    setFragmentAudioTime={setFragmentAudioTime}
+                                />
+                            </div>
+                        </div>
+                    )}
                 </div>
             </main>
+
+            {/* Footer */}
+            <footer className="bg-white border-t border-gray-200 py-4">
+                <div className="container mx-auto text-center text-sm text-gray-500">
+                    <p>
+                        &copy; {new Date().getFullYear()} {titlePage}. All
+                        rights reserved.
+                    </p>
+                </div>
+            </footer>
         </div>
     );
 }
