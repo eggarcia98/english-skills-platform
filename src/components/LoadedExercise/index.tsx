8d2d78b0fee4548d54a662a3b6b81152a647f7fc
Erick Garcia <eggarcia9814@gmail.com>
2024-11-05 14:33:28 +1000

    improving previous exercises list

--- src/components/LoadedExercise/index.tsx
+++ src/components/LoadedExercise/index.tsx
@@ -22,17 +22,21 @@ export default function LoadedExercise() {
         <div className="my-16 text-left grid grid-cols-1">
             <h1>Try Exercises performed by other students</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 my-8 max-h-[calc(3*5rem)] overflow-y-scroll">
-                {loadedExercises.map(({ name, duration }: any) => (
-                    <button className="grid grid-cols-[auto_1fr] border rounded-lg w-3/4 sm:w-full items-center justify-self-center">
-                        <div className="px-4 border-r flex flex-col ">PLAY</div>
-                        <div className="p-2 px-4 flex flex-col items-start  overflow-hidden">
-                            <div className="truncate font-semibold ">
-                                {name}
+                {loadedExercises.map(
+                    ({ name, duration }: any, index: number) => (
+                        <button className="grid grid-cols-[auto_1fr] border rounded-lg w-3/4 sm:w-full items-center justify-self-center">
+                            <div className="px-4 border-r flex flex-col ">
+                                PLAY
                             </div>
-                            <div className="text-gray-500"> {duration}</div>
-                        </div>
-                    </button>
-                ))}
+                            <div className="p-2 px-4 flex flex-col items-start  overflow-hidden">
+                                <div className="truncate font-semibold ">
+                                    {name}
+                                </div>
+                                <div className="text-gray-500"> {duration}</div>
+                            </div>
+                        </button>
+                    )
+                )}
             </div>
         </div>
     );
