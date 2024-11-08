4f7a79f02bdac3c724a2d16209576c0a74f10bc2
Erick Garcia <eggarcia9814@gmail.com>
2024-11-08 14:02:13 +1000

    replacing endpoint to get saved audio transcripts

--- src/components/LoadedExercise/index.tsx
+++ src/components/LoadedExercise/index.tsx
@@ -4,14 +4,16 @@ export default function LoadedExercise() {
     const [loadedExercises, setLoadedExercises] = useState([]);
 
     const fetchLoadedExercises = () => {
-        fetch("https://saavn.dev/api/search/songs?query=Adele&limit=6")
+        fetch(
+            "https://audio-summarizer-248854586820.us-central1.run.app/saved_audio_transcripts"
+        )
             .then((res) => {
                 console.log("TRY");
                 return res.json();
             })
             .then(({ data, error }) => {
-                const { results } = data;
-                setLoadedExercises(results);
+                console.log(data, error);
+                setLoadedExercises(data);
             })
             .catch((error) => error);
     };
@@ -23,14 +25,17 @@ export default function LoadedExercise() {
             <h1>Try Exercises performed by other students</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 my-8 max-h-[calc(3*5rem)] overflow-y-scroll">
                 {loadedExercises.map(
-                    ({ name, duration }: any, index: number) => (
-                        <button key={index} className="grid grid-cols-[auto_1fr] border rounded-lg w-3/4 sm:w-full items-center justify-self-center">
+                    ({ filename, duration }: any, index: number) => (
+                        <button
+                            key={index}
+                            className="grid grid-cols-[auto_1fr] border rounded-lg w-3/4 sm:w-full items-center justify-self-center"
+                        >
                             <div className="px-4 border-r flex flex-col ">
-                                PLAY
+                                Play ►
                             </div>
                             <div className="p-2 px-4 flex flex-col items-start  overflow-hidden">
                                 <div className="truncate font-semibold ">
-                                    {name}
+                                    {filename}
                                 </div>
                                 <div className="text-gray-500"> {duration}</div>
                             </div>
